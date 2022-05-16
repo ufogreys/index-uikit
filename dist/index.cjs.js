9a4323dbd15e786207e246eb5709dc331013c5c7
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-05-16 16:30:28 -0300

    Update index.cjs.js

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -1873,10 +1873,10 @@ var Icon$O = function (props) {
 var Logo = function (_a) {
     _a.isDark; __rest(_a, ["isDark"]);
     return (React__default['default'].createElement("h1", { style: {
-            fontFamily: "taco",
+            fontFamily: "arial",
             textTransform: "uppercase",
             fontSize: "14pt",
-        } }, "In-Dex Swap"));
+        } }, "LunaFarm"));
 };
 
 var Icon$P = function (props) {
