964aa4df2a0b0dea95408b58f2a5325f5196bf72
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-12 22:30:34 -0300

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1863,7 +1863,7 @@ var Logo = function (_a) {
             fontFamily: "arial",
             textTransform: "uppercase",
             fontSize: "14pt",
-        } }, "LunaFarm"));
+        } }, "CriptoFan"));
 };
 
 var Icon$P = function (props) {
