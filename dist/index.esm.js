a7f5d6882bab02175a4c668d3ac52570b786812b
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-14 04:29:41 -0300

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1861,7 +1861,7 @@ var Logo = function (_a) {
     _a.isDark; __rest(_a, ["isDark"]);
     return (React__default['default'].createElement("h1", { style: {
             fontFamily: "Anniversary-Neon"
-            fontSize: "20pt",
+            fontSize: "14pt",
         } }, "CryptoFAN Dex"));
 };
 
