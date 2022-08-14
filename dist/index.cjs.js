6618586adf534393325a77b8c849e80a125965f5
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-14 04:46:57 -0300

    Update index.cjs.js

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -1873,7 +1873,7 @@ var Icon$O = function (props) {
 var Logo = function (_a) {
     _a.isDark; __rest(_a, ["isDark"]);
     return (React.createElement("h1", { style: {
-            fontFamily: "Scum",
+            fontFamily: "Ostrich Sans",
             textTransform: "uppercase",
             fontSize: "14pt",
         } }, "CryptoFan Dex"));
