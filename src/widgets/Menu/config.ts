0f3d3921aa5549fce5d5b6b04794ed4f9778e35d
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-13 22:47:00 -0300

    Update config.ts

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -120,7 +120,7 @@ export const socials = [
     items: [
       {
         label: "English",
-        href: "https://t.me/criptofantoken",
+        href: "https://t.me/cryptofantoken",
       },
       // {
       //   label: "Bahasa Indonesia",
