68364463979dc541f59f8784cae2066c09f11e0e
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-14 05:37:39 -0300

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1858,9 +1858,12 @@ var Icon$O = function (props) {
 };
 
 var Logo = function (_a) {
-    var isDark = _a.isDark, props = __rest(_a, ["isDark"]);
-    return (React__default['default'].createElement(Svg, __assign({ viewBox: "0 0 205 26" }, props),
-        React__default['default'].createElement("image", { width: "205", height: "26", href: isDark ? '/images/egg/logoprincipal.png' : '/images/egg/logoprincipal.png' })));
+    _a.isDark; __rest(_a, ["isDark"]);
+    return (React.createElement("h1", { style: {
+            fontFamily: "Ostrich Sans",
+            textTransform: "uppercase",
+            fontSize: "14pt",
+        } }, "CryptoFan Dex"));
 };
 
 var Icon$P = function (props) {
