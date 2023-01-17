ced034d9b5085e5fe749708a21a6ac51a2c9c1ec
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-01-16 22:16:07 -0300

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1524,7 +1524,7 @@ var mediaQueries = {
 var shadows = {
     level1: "0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05)",
     active: "0px 0px 0px 1px #0098A1, 0px 0px 4px 8px rgba(31, 199, 212, 0.4)",
-    success: "0px 0px 0px 1px #31D0AA, 0px 0px 0px 4px rgba(49, 208, 170, 0.2)",
+    success: "0px 0px 0px 1px #00be00, 0px 0px 0px 4px rgba(49, 208, 170, 0.2)",
     warning: "0px 0px 0px 1px #ED4B9E, 0px 0px 0px 4px rgba(237, 75, 158, 0.2)",
     focus: "0px 0px 0px 1px #7645D9, 0px 0px 0px 4px rgba(118, 69, 217, 0.6)",
     inset: "inset 0px 2px 2px -1px rgba(74, 74, 104, 0.1)",
@@ -2731,11 +2731,11 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#31D0AA",
-    primaryBright: "#31D0AA",
-    primaryDark: "#31D0AA",
+    primary: "#00be00",
+    primaryBright: "#00be00",
+    primaryDark: "#00be00",
     secondary: "#7645D9",
-    success: "#31D0AA",
+    success: "#00be00",
     warning: "#FFB237",
 };
 var brandColors = {
